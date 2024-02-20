348d01b485d467fb457d729c8ba4e5e94fdec89f
Jun Yang <junyang168@gmail.com>
2024-02-20 22:52:06 +0800

    fix prerender error

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
     <div className="absolute inset-0 min-h-[500px] flex items-center justify-center">
       <div className="relative flex flex-col gap-8 px-4 -mt-24">
         <Logo></Logo>
-        <Search></Search>
+          <Search></Search>
         <div className="flex gap-2 flex-wrap justify-center">
           <PresetQuery query="When will ESXi 7 go out of support?"></PresetQuery>
           <PresetQuery query="Why do we only see one side of the moon?"></PresetQuery>
