1429a14e9a6a01508f1f512ab07413659b40b56b
Jun Yang <junyang168@gmail.com>
2024-02-20 22:03:03 +0800

    f

--- web/src/app/search/page.tsx
+++ web/src/app/search/page.tsx
@@ -3,6 +3,7 @@ import { Result } from "@/app/components/result";
 import { Search } from "@/app/components/search";
 import { Title } from "@/app/components/title";
 import { useSearchParams } from "next/navigation";
+import { Suspense } from "react";
 export default function SearchPage() {
   const searchParams = useSearchParams();
   const query = decodeURIComponent(searchParams.get("q") || "");
@@ -19,7 +20,9 @@ export default function SearchPage() {
         <div className="h-80 pointer-events-none w-full rounded-b-2xl backdrop-filter absolute bottom-0 bg-gradient-to-b from-transparent to-white [mask-image:linear-gradient(to_top,white,transparent)]"></div>
         <div className="absolute z-10 flex items-center justify-center bottom-6 px-4 md:px-8 w-full">
           <div className="w-full">
-            <Search></Search>
+            <Suspense>
+              <Search></Search>
+            </Suspense>
           </div>
         </div>
       </div>
