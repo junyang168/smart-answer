f0fa2f7ef254f388eb55f4107a77b830d4d9147e
Jun Yang <junyang168@gmail.com>
2025-10-05 04:57:39 -0500

    f

--- web/src/app/admin/layout.tsx
+++ web/src/app/admin/layout.tsx
@@ -6,7 +6,7 @@ import { authConfig } from "@/app/utils/auth";
 
 export default async function AdminLayout({ children }: { children: ReactNode }) {
   const session = await getServerSession(authConfig);
-/*
+
   if (!session) {
     redirect(`/api/auth/signin?callbackUrl=${encodeURIComponent("/admin")}`);
   }
@@ -14,7 +14,7 @@ export default async function AdminLayout({ children }: { children: ReactNode })
   if (session.user?.role !== "editor") {
     redirect("/");
   }
-*/
+
   return (
     <>
       <style>{`#global-site-header { display: none !important; }`}</style>
